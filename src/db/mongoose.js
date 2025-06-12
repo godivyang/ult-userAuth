96c06630a19a44377a65ea05d803f8b7cbc90670
godivyang <godivyang@users.noreply.github.com>
2025-06-12 12:32:21 +0530

    added logs to debug app in prod

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -8,6 +8,7 @@ const clientOptions = { serverApi: { version: '1', strict: true, deprecationErro
 async function run() {
   try {
     // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    console.log(uri)
     await mongoose.connect(uri, clientOptions);
     await mongoose.connection.db.admin().command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
