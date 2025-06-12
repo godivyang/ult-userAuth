f22a55682f2cb51e7c4540777f0c11a44d490f8f
godivyang <godivyang@users.noreply.github.com>
2025-06-12 14:23:15 +0530

    changed config for cookies part 5

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -8,10 +8,9 @@ const clientOptions = { serverApi: { version: '1', strict: true, deprecationErro
 async function run() {
   try {
     // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-    console.log(uri)
     await mongoose.connect(uri, clientOptions);
     await mongoose.connection.db.admin().command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await mongoose.disconnect();
