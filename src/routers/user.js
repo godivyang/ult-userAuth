cc4c48ed0abf0bc9aac507fee69e1e5a0b7fca81
godivyang <godivyang@users.noreply.github.com>
2025-06-22 05:59:50 +0530

    Testing login redirect part 10 retract

--- src/routers/user.js
+++ src/routers/user.js
@@ -28,7 +28,6 @@ router.post("/user/signup", async (req, res) => {
 });
 
 router.get("/user/me", auth, async (req, res) => {
-    res.cookie("token", token, tokenOptions);
     res.send(req.user);
 });
 
