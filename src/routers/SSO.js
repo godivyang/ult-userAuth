0dec6182635bdddc861210314eda191dba7e4bf6
godivyang <godivyang@users.noreply.github.com>
2025-06-22 21:10:22 +0530

    SSO feature implementation part 4

--- src/routers/SSO.js
+++ src/routers/SSO.js
@@ -6,7 +6,7 @@ const auth = require("../middleware/auth");
 router.get("/sso/crossAppLogin", auth, async (req, res) => {
     try {
         const sso = await SSO.generateSingleSignOnToken(req.user._id);
-
+console.log(sso)
         res.send(sso);
     } catch (e) {
         res.send(e);
