da22255e4708248a460bc1156f67ee70fa8a01c6
godivyang <godivyang@users.noreply.github.com>
2025-07-15 22:50:28 +0530

    SSO issue resolution 2

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -11,7 +11,7 @@ const auth = async (req, res, next) => {
         const decoded = jwt.verify(token, process.env.JWT_Secret);
         console.log("decoded", decoded)
         const user = await User.findOne({ _id: decoded._id, "tokens.token": token });
-// console.log("user from auth.js", user);
+console.log("user from auth.js", user);
         if(!user) {
             throw new Error();
         }
