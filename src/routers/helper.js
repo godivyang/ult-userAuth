3c46116d80575a7e5fc0eb79fbf805c231831828
godivyang <godivyang@users.noreply.github.com>
2025-08-07 02:29:05 +0530

    resolving guest login issue in typing bliss

--- src/routers/helper.js
+++ src/routers/helper.js
@@ -3,22 +3,13 @@ const router = new express.Router();
 const helperAuth = require("../middleware/helperAuth");
 const admin = require('firebase-admin');
 
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64, 'base64').toString('utf8')
+);
+
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert({
-    "type": process.env.FIREBASE_TYPE,
-    "project_id": process.env.FIREBASE_PROJECT_ID,
-    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-    "private_key": process.env.FIREBASE_PRIVATE_KEY,
-    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-    "client_id": process.env.FIREBASE_CLIENT_ID,
-    "auth_uri": process.env.FIREBASE_AUTH_URI,
-    "token_uri": process.env.FIREBASE_TOKEN_URI,
-    "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL,
-    "universe_domain": process.env.FIREBASE_UNIVERSE_DOMAIN
-  }),
-    databaseURL: process.env.FIREBASE_DATABASE_URL
+    credential: admin.credential.cert(serviceAccount)
   });
 }
 
