0ba8ed045c2e3f5dfe7601f2a98c3106e6cf8d70
godivyang <godivyang@users.noreply.github.com>
2025-09-01 20:52:46 +0530

    Bug fix

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -9,6 +9,7 @@ const auth = async (req, res, next) => {
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         // const user = await User.findOne({ _id: decoded._id, "tokens.token": token });
+        console.log(decoded)
         const user = {_id: decoded._id, name: decoded.name, email: decoded.email}
 
         if(!user) {
